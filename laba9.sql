589b92169b523e58c52a1b03a2c070b3c97e04dd
Wistrekz <96283348+Wistrekz@users.noreply.github.com>
2022-11-29 11:10:21 +0300

    Add files via upload

--- laba9.sql
+++ laba9.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 29 2022 г., 00:09
--- Версия сервера: 10.3.22-MariaDB
--- Версия PHP: 7.4.5
+-- Время создания: Ноя 29 2022 г., 11:09
+-- Версия сервера: 8.0.24
+-- Версия PHP: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `laba9`
+-- База данных: `laba9pr`
 --
 
 -- --------------------------------------------------------
@@ -28,19 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `table1` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int NOT NULL,
+  `name` varchar(1000) NOT NULL,
+  `text` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `table1`
 --
 
 INSERT INTO `table1` (`id`, `name`, `text`) VALUES
-(1, 'вова', 'вапувпвапвапвыапвыап'),
-(2, 'киря', 'выаваиапраоа'),
-(3, 'don', 'edsrsdfsdfd');
+(1, 'вова', 'гнпргшппогпгш'),
+(2, 'вова', 'гнпргшппогпгш'),
+(3, 'миша', 'иеениении'),
+(4, 'алексей', 'иеривсвысывыв');
 
 -- --------------------------------------------------------
 
@@ -49,19 +50,48 @@ INSERT INTO `table1` (`id`, `name`, `text`) VALUES
 --
 
 CREATE TABLE `table2` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int NOT NULL,
+  `text` varchar(1000) NOT NULL,
+  `name` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `table2`
 --
 
-INSERT INTO `table2` (`id`, `name`, `text`) VALUES
-(27, 'вова', 'вапувпвапвапвыапвыап'),
-(28, 'киря', 'выаваиапраоа'),
-(29, 'don', 'edsrsdfsdfd');
+INSERT INTO `table2` (`id`, `text`, `name`) VALUES
+(3, 'гнпргшппогпгш\n', 'вова'),
+(4, 'гнпргшппогпгш\n', 'вова'),
+(5, 'иеривсвысывыв\n', 'алексей'),
+(6, 'иеривсвысывыв\n', 'алексей'),
+(7, 'гнпргшппогпгш\n', 'вова'),
+(8, 'иеривсвысывыв\n', 'алексей'),
+(9, 'иеривсвысывыв\n', 'алексей'),
+(10, '', ''),
+(11, '', ''),
+(12, 'иеривсвысывыв\n', 'алексей'),
+(13, 'иеривсвысывыв\n', 'алексей'),
+(14, 'иеривсвысывыв\n', 'алексей'),
+(15, 'Array\n', 'Array'),
+(16, 'иеривсвысывыв\n', 'алексей'),
+(17, 'иеривсвысывыв\n', 'алексей'),
+(18, 'иеривсвысывыв<br>', 'алексей'),
+(19, 'иеривсвысывыв</br>', 'алексей'),
+(20, 'иеривсвысывыв\n', 'алексей'),
+(21, 'иеривсвысывыв\n', 'алексей'),
+(22, 'гнпргшппогпгш<br>вова', 'вова'),
+(23, 'иеениении<br>алексей', 'гнпргшппогпгш<br>миша'),
+(24, '', 'иеривсвысывыв<br>'),
+(25, 'гнпргшппогпгш<br>вова', 'вова'),
+(26, 'иеениении<br>алексей', 'гнпргшппогпгш<br>миша'),
+(27, '', 'иеривсвысывыв<br>'),
+(28, 'гнпргшппогпгш\nвова', 'вова'),
+(29, 'иеениении\nалексей', 'гнпргшппогпгш\nмиша'),
+(30, '', 'иеривсвысывыв\n'),
+(31, 'гнпргшппогпгш', 'вова'),
+(32, 'гнпргшппогпгш', 'вова'),
+(33, 'иеениении', 'миша'),
+(34, 'иеривсвысывыв', 'алексей');
 
 --
 -- Индексы сохранённых таблиц
@@ -87,13 +117,13 @@ ALTER TABLE `table2`
 -- AUTO_INCREMENT для таблицы `table1`
 --
 ALTER TABLE `table1`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `table2`
 --
 ALTER TABLE `table2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
